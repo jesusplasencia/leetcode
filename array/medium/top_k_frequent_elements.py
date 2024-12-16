57ce6ca4270c42d6728b5105916374c955a58b89
living-man <jesus.plasenciatoledo@contractors.lesschwab.com>
2024-12-16 11:02:59 -0500

    Top K Frequent Items

--- array/medium/top_k_frequent_elements.py
+++ array/medium/top_k_frequent_elements.py
@@ -1,8 +1,36 @@
+import heapq;
 from typing import List;
 from collections import Counter;
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        c1 = Counter(nums);
-        n = len(c1.keys())
-        return c1.keys()[n-k:n];
+        counter = Counter(nums);
+        heap = [];
+
+        for key, val in counter.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (val, key))
+            else:
+                heapq.heappushpop(heap, (val, key))
+    
+        return [h[1] for h in heap]
+
+    def topKFrequentV1(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums);
+        counter = Counter(nums);
+        buckets = [0] * (n + 1)
+
+        for num, freq in counter.items():
+            if buckets[freq] == 0:
+                buckets[freq] = [num]
+            else:
+                buckets[freq].append(num)
+
+        res = []
+        for i in range(n, -1, -1):
+            if buckets[i] != 0:
+                res.extend(buckets[i])
+            if len(res) == k:
+                break;
+
+        return res
