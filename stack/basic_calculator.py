f274fffa07a116f2140fd7462094d54c9c417602
eggman <100390233+JesusPlasencia@users.noreply.github.com>
2024-12-23 23:29:13 -0500

    Stack leetcode 150

--- stack/basic_calculator.py
+++ stack/basic_calculator.py
@@ -1,9 +1,28 @@
 class Solution:
-    def calculate(self, s: str, res: int = 0) -> int:
-        split_mult = s.split("*");
-
-        return calculate();
+    def calculate(self, s: str) -> int:
+        output,curr,sign,stk = 0,0,1,[]
+        for c in s:
+            if c.isdigit():
+                curr = curr*10 + int(c);
+            elif c in "+-":
+                output += (curr*sign);
+                curr = 0;
+                if c == "+":
+                    sign = 1;
+                else:
+                    sign = -1;
+            elif c == "(":
+                stk.append(output);
+                stk.append(sign);
+                output = 0;
+                sign = 1;
+            elif c == ")":
+                output += (curr*sign);
+                output *= stk.pop();
+                output += stk.pop();
+                curr = 0;
+        return output + (curr * sign);
 
 if __name__ == "__main__":
     sol = Solution();
-    print(sol.calculate("3+2*2")); # 7
+    print(sol.calculate("(1+(4+5+2)-3)+(6+8)"));
